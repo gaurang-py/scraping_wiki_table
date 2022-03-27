7ca3f6da0acdeb5901fd8c8e87b06cdab7fafb2a
GAURANG PATEL <72779410+gaurang-py@users.noreply.github.com>
2022-03-27 22:57:09 +0530

    Update wiki.py

--- wiki.py
+++ wiki.py
@@ -1,12 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-wiki_link = "https://en.wikipedia.org/wiki/List_of_cryptocurrencies"
-page = requests.get(wiki_link)
-table_class = "wikitable sortable jquery-tablesorter"
-soup = BeautifulSoup(page.text, 'html.parser')
-all_tables = soup.find_all('table')
-print(str(all_tables).encode('utf8'))
-df=pd.read_html(str(all_tables))
-df.to_csv("SCRAPED.csv")
-print(df)
+try:
+  wiki_link = "https://en.wikipedia.org/wiki/List_of_cryptocurrencies"
+  page = requests.get(wiki_link)
+  table_class = "wikitable sortable jquery-tablesorter"
+  soup = BeautifulSoup(page.text, 'html.parser')
+  all_tables = soup.find_all('table')
+  print(str(all_tables).encode('utf8'))
+  df=pd.read_html(str(all_tables))
+  df.to_csv("SCRAPED.csv")
+  print(df)
+except:
+  print("Error")
